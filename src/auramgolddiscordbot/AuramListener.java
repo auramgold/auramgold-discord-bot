6096df286b2bb0b4a6f05e5a2b26607c5c11b568
auramgold <User@10.12.238.126>
2018-05-18 11:56:07 -0400

    Added a feature to link referenced SCP articles.

--- src/auramgolddiscordbot/AuramListener.java
+++ src/auramgolddiscordbot/AuramListener.java
@@ -5,14 +5,23 @@
  */
 package auramgolddiscordbot;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.core.requests.restaction.MessageAction;
 
 /**
  * This class listens for events and handles them.
@@ -70,7 +79,50 @@ public class AuramListener extends ListenerAdapter
 						? "<https://youtu.be/0DLzyvT4eUo?t=43s>" 
 						: "<https://youtu.be/79DijItQXMM?t=38s>")).queue();
 		}
+		else if(content.toLowerCase().contains("scp-"))
+		{
+			String regex = "scp\\-(\\S+)";
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(content.toLowerCase());
+			
+			boolean foundResult = false;
+
+			String mess = "Excuse me, but I found some SCPs in your message that I can link to:";
+			while (matcher.find())
+			{
+				String link = "http://www.scp-wiki.net/scp-" + matcher.group(1);
+				boolean isPage = false;
+				try
+				{
+					isPage = checkPageExists(link);
+				}
+				catch (Exception ex)
+				{
+					Logger.getLogger(AuramListener.class.getName()).log(Level.SEVERE, null, ex);
+				}
+				
+				
+				if(isPage)
+				{
+					foundResult = true;
+					mess += "\r\n" + link;
+				}
+			}
+
+			if(foundResult)
+			{	
+				channel.sendMessage(mess).queue();
+			}
+		}
 	}
 	
+	static boolean checkPageExists(String link) throws MalformedURLException, IOException
+	{
+		URL url = new URL(link);
+		HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+		connect.setRequestMethod("HEAD");
+		int status = connect.getResponseCode();
+		return status != 404;
+	}
 	
 }
